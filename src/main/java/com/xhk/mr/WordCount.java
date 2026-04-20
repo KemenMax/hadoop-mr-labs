3fa9891d202fba75781404f3c6ccb012005d4c1c
Max <xionghoukai0729@outlook.com>
2026-04-20 11:25:21 +0800

    wc首次上传，未完成

---
+++ src/main/java/com/xhk/mr/WordCount.java
@@ -0,0 +1,17 @@
+package com.xhk.mr;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+
+public class WordCount {
+    public static class TokenizerMapper extends Mapper<Object, Text, Text, IntWritable> {
+        private final static IntWritable one = new IntWritable(1);
+        private Text word = new Text();
+        public void map(Object key, Text value, Context context) throws IOException, InterruptedException{
+
+        }
+    }
+}
